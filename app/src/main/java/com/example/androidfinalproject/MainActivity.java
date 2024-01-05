aab08c2e54f22ee485db997c782186d40db7e862
Byron Cox <cox00144@algonquinlive.com>
2024-01-04 22:20:32 -0500

    Added Database opener class.

--- app/src/main/java/com/example/androidfinalproject/MainActivity.java
+++ app/src/main/java/com/example/androidfinalproject/MainActivity.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends home {
 
     // extend BaseActivity to add toolbar functions to Activity
     @Override
