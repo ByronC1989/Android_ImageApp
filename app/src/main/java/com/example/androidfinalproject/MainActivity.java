ff7a54fce112ab7ee297a34a5d6473bc3dd76e09
Alycia <alycia.curry01@gmail.com>
2024-01-25 09:36:48 -0500

    commit

--- app/src/main/java/com/example/androidfinalproject/MainActivity.java
+++ app/src/main/java/com/example/androidfinalproject/MainActivity.java
@@ -1,18 +1,20 @@
 package com.example.androidfinalproject;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -29,6 +31,8 @@ public class MainActivity extends BaseActivity {
         Toolbar toolbar = findViewById(R.id.home_toolbar);
         setSupportActionBar(toolbar);
 
+        Button helpButton = findViewById(R.id.btn_help);
+        helpButton.setOnClickListener(v -> showHelpDialog());
         DrawerLayout drawerLayout = findViewById(R.id.homedrawer);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
                 drawerLayout, toolbar, R.string.open, R.string.close);
@@ -112,5 +116,17 @@ public class MainActivity extends BaseActivity {
         }
 
         return true;
+    }
+    private void showHelpDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Help");
+        builder.setMessage("Explanation for each program:\n\n"
+                + "1. Image of the Day: Enter a date to view or save the NASA image of the day!\n\n"
+                + "2. Picture Roulette: Picture roulette selects a random date to view!\n\n"
+                + "3. Favourites: View the previously selected favourites!");
+
+        builder.setPositiveButton("OK", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
